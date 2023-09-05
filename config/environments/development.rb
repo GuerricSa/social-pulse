23ef3c4b60dde2c7fb333dc9022e9bf01f4fc1f4
guerric sant <guerric.sant@gmail.com>
2023-09-05 11:18:17 +0200

    dev.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,6 +18,10 @@ Rails.application.configure do
   # Enable server timing
   config.server_timing = true
 
+  # Allow requests localtunnel
+  config.hosts.clear
+
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join("tmp/caching-dev.txt").exist?
