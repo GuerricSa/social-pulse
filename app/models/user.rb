adfa4da88eaef580c57bdf0faa0899e380ff97eb
Cyrielle Marchand <cyrielle.marchand@gmail.com>
2023-09-05 11:45:10 +0200

    validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :first_name, :age, :email, :password, presence: true
+  validates :age, numericality: { only_integer: true, in: 15..100 }
+  validates :presentation, length: { maximum: 150 }
+  validates :email, uniqueness: true
 end
