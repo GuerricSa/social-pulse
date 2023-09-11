67710330aaa5923497aa556c221850815c43bf30
Cyrielle Marchand <cyrielle.marchand@gmail.com>
2023-09-11 16:55:04 +0200

    improved the sweet alert for registration

--- app/javascript/controllers/register_controller.js
+++ app/javascript/controllers/register_controller.js
@@ -4,12 +4,7 @@ import Swal from "sweetalert2"
 // Connects to data-controller="register"
 export default class extends Controller {
   static values = {
-    icon: String,
-    alertTitle: String,
-    alertHtml: String,
-    confirmButtonText: String,
-    showCancelButton: Boolean,
-    cancelButtonText: String
+    activityId: String
   }
 
   initSweetalert(event) {
@@ -17,34 +12,23 @@ export default class extends Controller {
 
     Swal.fire({
       title: 'Confirmes-tu ta participation ?',
-      showDenyButton: true,
-      showCancelButton: false,
-      confirmButtonText: 'Je confirmes ma participation',
-      denyButtonText: `Annuler`,
-    }).then((action) => {
-      /* Read more about isConfirmed, isDenied below */
-      if (action.isConfirmed) {
-        Swal.fire('Ta participation est enregistrée !', '', 'success');
+      text: "La personne qui propose cette activité sera avertie de ta participation.",
+      icon: 'question',
+      showCancelButton: true,
+      // confirmButtonColor: '#3085d6',
+      // cancelButtonColor: '#d33',
+      confirmButtonText: 'Oui, je confirme',
+      cancelButtonText: 'Annuler'
+    }).then((result) => {
+      if (result.isConfirmed) {
         event.target[event.type]();
-      } else if (action.isDenied) {
-        Swal.fire('Aucune participation enregistrée', '', 'info')
+        Swal.fire(
+          'Incription validée !',
+          "Ta participation a bien été enregistrée.",
+          'success'
+        );
       }
     })
-    .catch(event.preventDefault())
-
-    // Swal.fire({
-    //   icon: this.iconValue,
-    //   title: this.titleValue,
-    //   html: this.htmlValue,
-    //   confirmButtonText: this.confirmButtonTextValue,
-    //   showCancelButton: this.showCancelButtonValue,
-    //   cancelButtonText: this.cancelButtonTextValue,
-    //   reverseButtons: true
-    // }).then((action) => {
-    //   if (action.isConfirmed) {
-    //     event.target[event.type](); // "submit"
-    //   }
-    // })
-    // .catch(event.preventDefault())
+    .catch(event.preventDefault());
   }
 }
