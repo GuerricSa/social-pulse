3a7275ea9f8443a469d874abebeb13df2b645ee0
kilian le doeuff <kilian78000@yahoo.fr>
2023-09-11 17:01:44 +0200

    début de report

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_07_125755) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_11_081549) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -87,6 +87,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_07_125755) do
     t.index ["user_id"], name: "index_registrations_on_user_id"
   end
 
+  create_table "reports", force: :cascade do |t|
+    t.string "content"
+    t.string "status"
+    t.bigint "reporter_id"
+    t.bigint "defendant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["defendant_id"], name: "index_reports_on_defendant_id"
+    t.index ["reporter_id"], name: "index_reports_on_reporter_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -108,4 +119,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_07_125755) do
   add_foreign_key "activities", "users"
   add_foreign_key "registrations", "activities"
   add_foreign_key "registrations", "users"
+  add_foreign_key "reports", "users", column: "defendant_id"
+  add_foreign_key "reports", "users", column: "reporter_id"
 end
