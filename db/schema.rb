0aea51fd1b71861a76332c1deefd71898324b039
Cyrielle Marchand <cyrielle.marchand@gmail.com>
2023-09-12 17:32:45 +0200

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_12_073707) do
-
+ActiveRecord::Schema[7.0].define(version: 2023_09_12_130032) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -107,6 +106,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_12_073707) do
     t.datetime "updated_at", null: false
     t.index ["read_at"], name: "index_notifications_on_read_at"
     t.index ["recipient_type", "recipient_id"], name: "index_notifications_on_recipient"
+  end
 
   create_table "read_marks", force: :cascade do |t|
     t.string "readable_type", null: false
